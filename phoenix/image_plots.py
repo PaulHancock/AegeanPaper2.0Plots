82feaebc8bbd226e889015f6fed947bab9b3451d
PaulHancock <paul.hancock@curtin.edu.au>
2017-05-03 14:01:49 +0800

    fix intensity scale to be in mJy

--- phoenix/image_plots.py
+++ phoenix/image_plots.py
@@ -2,10 +2,12 @@ from astropy.io import fits
 import aplpy
 
 phoenix = fits.open('1997.fits')
+phoenix[0].data *= 1e3
 sim = fits.open('../MapF00E07.fits')
+sim[0].data *= 1e3
 
 f = aplpy.FITSFigure(phoenix)
-f.show_grayscale(vmin=-2e-4, vmax=1e-3, invert=True)
+f.show_grayscale(vmin=-0.2, vmax=1, invert=True)
 f.show_colorbar()
 f.colorbar.set_axis_label_text('mJy/beam')
 f.colorbar.set_axis_label_font(size=14)
@@ -15,7 +17,7 @@ f.axis_labels.set_font(size=14)
 f.save('phoenix.png')
 
 f = aplpy.FITSFigure(sim)
-f.show_grayscale(vmin=-2e-4, vmax=1e-3, invert=True)
+f.show_grayscale(vmin=-0.2, vmax=1, invert=True)
 f.show_colorbar()
 f.colorbar.set_axis_label_text('mJy/beam')
 f.colorbar.set_axis_label_font(size=14)
