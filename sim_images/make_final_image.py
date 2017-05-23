d93132adbde5f3734b2701397ae1ef00b26cdf5d
PaulHancock <paul.hancock@curtin.edu.au>
2017-05-23 15:24:05 +0800

    add comments/progress reporting

--- sim_images/make_final_image.py
+++ sim_images/make_final_image.py
@@ -9,19 +9,29 @@ from scipy.ndimage import gaussian_filter
 
 def main():
     # Load background image and normalise to be 0-1
-    background = gaussian_filter(fits.open('moon.fits')[0].data/255., sigma=150)
+    print "loading moon.fits"
+    moon = fits.open('moon.fits')
+    print "smoothing and re-scaling"
+    background = gaussian_filter(moon[0].data/255., sigma=150)
     background -= np.min(background)
     background /= np.max(background)
     # background to have positive and negative components, with a magnitude of 10\sigma
     background = (background-0.5) * 10 * 0.1
+    moon[0].data = background
+
+    print "saving background.fits"
+    moon.writeto('background.fits', overwrite=True)
+
+    print "loading noise.fits/model.fits"
     # load the noise image
     rms = fits.open('noise.fits')[0].data
 
     # load the image with the sources in it
     model = fits.open('model.fits')
+    print "constructing simulated image"
     model[0].data += background + rms
     model.writeto('SimulatedImage.fits', overwrite=True)
-
+    print "done"
 
 if __name__ == "__main__":
     main()
