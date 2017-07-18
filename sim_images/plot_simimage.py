56b0644c29c1a1f756cc92684fce1d38ebd659ce
PaulHancock <paul.hancock@curtin.edu.au>
2017-07-18 14:21:39 +0800

    invert colormap and line/test colors

--- sim_images/plot_simimage.py
+++ sim_images/plot_simimage.py
@@ -9,7 +9,7 @@ sim = fits.open('SimulatedImage.fits')
 fig = pyplot.figure(figsize=(10,10))
 wcs = WCS(sim[0].header)
 ax = fig.add_subplot(1,1,1,projection=wcs)
-mpbl = ax.imshow(sim[0].data, origin='lower', vmin=-0.4, vmax=1, cmap='gray')
+mpbl = ax.imshow(sim[0].data, origin='lower', vmin=-0.2, vmax=1, cmap='gray_r')
 cax = fig.colorbar(mpbl, shrink=0.80)
 
 cax.set_label("Flux density (Jy/beam)", fontsize=12)
@@ -18,10 +18,9 @@ ax.coords['ra'].set_ticks(np.arange(0,360,30)*u.degree)
 ax.coords['ra'].set_ticklabel_visible(False)
 ax.coords['dec'].set_ticks(np.arange(-90,30,30)*u.degree)
 ax.coords['dec'].set_ticklabel_visible(False)
-ax.grid(zorder=2)
+ax.grid(zorder=2, color='black')
 
-fmt = {'color':'white', 'fontsize':12, 'xycoords':ax.get_transform('world'),
-      'backgroundcolor':'black', 'zorder':4}
+fmt = {'color':'black', 'fontsize':12, 'xycoords':ax.get_transform('world')}
 
 for ra in range(4,21,2):
     ax.annotate('{0:d}h'.format(ra),xy=[ra*15,-45], **fmt)
